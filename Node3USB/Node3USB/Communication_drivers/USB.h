c7c44ee17f4687a87d49ac9f594efc87d184f30d
unknown <sverrevr@itk-d659.win.ntnu.no>
2015-11-10 13:40:38 +0100

    Begynnt på USB veldig lite peiling

---
+++ Node3USB/Node3USB/Communication_drivers/USB.h
@@ -0,0 +1,16 @@
+/*
+ * USB.h
+ *
+ * Created: 10.11.2015 09:40:10
+ *  Author: sverrevr
+ */ 
+
+
+#ifndef USB_H_
+#define USB_H_
+
+
+
+
+
+#endif /* USB_H_ */
---
+++ Node3USB/Node3USB/Node3.c
@@ -0,0 +1,15 @@
+/*
+ * Node3.c
+ *
+ * Created: 10.11.2015 09:35:38
+ *  Author: sverrevr
+ */ 
+
+
+int main(){
+	
+	while(1){
+		
+	}
+	return 0;
+}
