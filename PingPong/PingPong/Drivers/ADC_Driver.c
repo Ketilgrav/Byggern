c2ef5fec19fd1577b213279995ba59189d7b05e3
Ketil <ketilgrav@gmail.com>
2015-11-19 13:31:06 +0100

    Fikset defines

--- PingPong/PingPong/Drivers/ADC_Driver.c
+++ PingPong/PingPong/Drivers/ADC_Driver.c
@@ -8,22 +8,21 @@
 #include "ADC_Driver.h"
 
 void ADC_init(){
-	clear_bit(INTR_DDR,INTR_bit);
+	clear_bit(INTR_DDR, INTR_BIT);
 }
 
 //Channel 0-3. (tilsvarer 1-4 i datablad, 0indekserte dem)
 uint8_t ADC_convert(char channel){
-	
 	//ch1 = 0100, ch2 = 0101, ch3 = 0110, ch4 = 0111, dette er lsb
 	if(channel > 3) channel = 3;
 	else if(channel< 0) channel = 0;
 	
-	*ADC_memory = ADC_single_ended | channel;
+	*ADC_MEMORY = ADC_SINGLE_ENDED | channel;
 	
-	//Venter på svar
-	while(read_bit(INTR_PIN,INTR_bit));
+	//Venter pÃ¥ svar
+	while(read_bit(INTR_PIN, INTR_bit));
 	
-	return *ADC_memory;
+	return *ADC_MEMORY;
 	
 }
 
