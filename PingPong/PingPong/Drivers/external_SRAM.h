3ff0966f51cb393626ed14c050bd555e82f95543
Ketil <ketilgrav@gmail.com>
2015-11-19 13:38:50 +0100

    Update external_SRAM.h

--- PingPong/PingPong/Drivers/external_SRAM.h
+++ PingPong/PingPong/Drivers/external_SRAM.h
@@ -10,6 +10,9 @@
 #ifndef EXTERNAL_SRAM_H_
 #define EXTERNAL_SRAM_H_
 
+#define RAM_START_ADR 0x1800
+#define EXT_RAM_SIZE 0x800 //2048 = 2^11. We have 11 address lines, so we can access 2048 memory locations.
+
 void SRAM_init(void);
 void SRAM_test(void);
 
