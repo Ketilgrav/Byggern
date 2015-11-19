9a9f0eb8eefb12999fdeaf892e9f9c213600b3fa
Ketil <ketilgrav@gmail.com>
2015-11-19 14:45:10 +0100

    Update can.c
    kommenterte ut memset, mulig det gir bugs

--- PingPong/PingPong/Communication_drivers/can.c
+++ PingPong/PingPong/Communication_drivers/can.c
@@ -11,7 +11,7 @@
 #include <string.h>
 
 void CAN_init(){
-	clear_bit(interrupt_DDR, interrupt_bit); //Input on the interrupt pin
+	clear_bit(INTERRUPT_DDR, INTERRUPT_BIT); //Input on the interrupt pin
 	
 	uint8_t value;
 	SPI_init(); // Initialize SPI
@@ -24,39 +24,39 @@ void CAN_init(){
 	}
 		
 	//Sets up RXBOCTRL
-		//RXM = 01, aktiv filter, kun kort id. 
+		//RXM = 01, activate filter, only short ID
 	mcp2515_bit_modify(MCP_RXB0CTRL, MCP_RXB0RXM_MASK, 1 << 5);
-		//BUKT: 1 -> ting går til RXB1 når denne er full
+		//BUKT: 1 -> transfers to RXB1 when RXB0 is full
 	mcp2515_bit_modify(MCP_RXB0CTRL, MCP_RXB0BUKT_MASK, 1 << 2);
 	//RXB1CTRL
-		//RXM = 01, aktiv filter, kun kort id. 
+		//RXM = 01, activate filter, only short ID
 	mcp2515_bit_modify(MCP_RXB1CTRL, MCP_RXB1RXM_MASK, 1 << 5);
 	
-	//Setter interrupt enable
+	//Set interrupt enable
 	//MERRE = 0 Message error interrupt
-	//WAKIE = 0 (har ikke sleep)
-	//ERRIE = 1 Error interrupt (kommer av flere)
+	//WAKIE = 0 (We dont use sleep)
+	//ERRIE = 1 Error interrupt 
 	//TX2IE = 0 Transmit 2 empty interrupt
 	//TX1IE = 0 Transmit 1 empty interrupt
 	//TX0IE = 0 Transmit 0 empty interrupt
-	//RX1IE = 1 Interrupt når data på RX1
-	//RX0IE = 1 -||- RX2
+	//RX1IE = 1 Interrupt when there is data RX1
+	//RX0IE = 1 Interrupt when there is data RX2
 	mcp2515_write(MCP_CANINTE, 0b00100011);
 	
-	//Filtere:
-	//Masken til RX0
-	mcp2515_write(MCP_RXM0SIDH, NODE1_CANID_H_mask >> 3);
-	mcp2515_bit_modify(MCP_RXM0SIDL, 0b11100000U, NODE1_CANID_H_mask << 5);
+	//Filters:
+	//Mask for RX0
+	mcp2515_write(MCP_RXM0SIDH, NODE1_CANID_H_MASK >> 3);
+	mcp2515_bit_modify(MCP_RXM0SIDL, 0b11100000U, NODE1_CANID_H_MASK << 5);
 	
-	//Masken til RX1
+	//Mask for RX1
 	mcp2515_write(MCP_RXM1SIDH, NODE1_CANID_L_mask >> 3);
-	mcp2515_bit_modify(MCP_RXM1SIDL, 0b11100000U, NODE1_CANID_L_mask << 5);
+	mcp2515_bit_modify(MCP_RXM1SIDL, 0b11100000U, NODE1_CANID_L_MASK << 5);
 	
-	//Filter 0 (RX0, går til RX1 om  RX0 er full)
+	//Filter 0 (RX0, goes to RX1 if RX0 is full)
 	mcp2515_write(MCP_RXF0SIDH, NODE1_CANID_HIGHPRIO_0 >> 3);
 	mcp2515_bit_modify(MCP_RXF0SIDL, 0b11100000U, NODE1_CANID_HIGHPRIO_0 << 5);
 	
-	//Filter 1 (RX0, går til RX1 om  RX0 er full)
+	//Filter 1 (RX0, goes to RX1 if RX0 is full)
 	mcp2515_write(MCP_RXF1SIDH, NODE1_CANID_HIGHPRIO_1 >> 3);
 	mcp2515_bit_modify(MCP_RXF1SIDL, 0b11100000U, NODE1_CANID_HIGHPRIO_1 << 5);
 	
@@ -81,14 +81,13 @@ void CAN_init(){
 }
 
 
-/*Returnerer 1 om meldignen ble sendt, ellers 0*/
+//Returns 1 if message was sedt, otherwise 0
 uint8_t CAN_message_send(CAN_message const * const msg){
-	//Finner hvilket register som er ledig
+	//Finds an available register
 	
 	//reg0, 0x30: status bit nr 2 
 	//reg1, 0x40: status bit nr 4 
 	//reg2, 0x50: status bit nr 6
-	
 	
 	uint8_t status = mcp2515_read_status();
 	if(status | (1 << 2)){
@@ -107,10 +106,10 @@ uint8_t CAN_message_send(CAN_message const * const msg){
 }
 
 void CAN_message_send_to_reg(CAN_message const * const msg, uint8_t reg){
-	//mcp2515_bit_modify(reg, TXP_MASK, msg->priority); //Setter prio
-	mcp2515_write(reg + 1, msg->id>>3); //Setter de 8 msb av id
-	mcp2515_write(reg + 2, msg->id<<5);	//De 3 lsb av id
-	mcp2515_write(reg + 5, msg->length);	//Data length. Kan sette RTR ved å | med 0b01000000
+	//mcp2515_bit_modify(reg, TXP_MASK, msg->priority); 	//Set priority
+	mcp2515_write(reg + 1, msg->id>>3); 			//Set 8 MSB of ID
+	mcp2515_write(reg + 2, msg->id<<5);			//Set 3 LSB of ID
+	mcp2515_write(reg + 5, msg->length);			//Data length. Can set RTR by | with 0b01000000
 	
 	for(uint8_t i = 0; i<msg->length; ++i){
 		mcp2515_write(reg + i+6, msg->data[i]);
@@ -126,21 +125,8 @@ void CAN_message_send_to_reg(CAN_message const * const msg, uint8_t reg){
 	}
 }
 
-/*
-bool CAN_data_receive(CAN_message* msg, interrupt intr){
-	if(intr = RX0){
-		return CAN_data_receive(MCP_RXB0CTRL);
-	}
-	else if( intr = RX1){
-		return CAN_data_receive(MCP_RXB1CTRL);
-	}
-	return false;
-}
-*/
-
 bool CAN_data_receive(CAN_message* msg, uint8_t reg){
-	
-	memset(msg, 0, sizeof(CAN_message));
+	//memset(msg, 0, sizeof(CAN_message));
 	
 	uint8_t retVal = false;
 	//reg0, 0x60: bit nr 0
@@ -160,18 +146,16 @@ bool CAN_data_receive(CAN_message* msg, uint8_t reg){
 }
 
 interrupt CAN_int(){
-	if(!read_bit(interrupt_PIN, interrupt_bit)){
-		//printf("Interupt active");
+	if(!read_bit(INTERRUPT_PIN, INTERRUPT_BIT)){
 		interrupt retval = (interrupt)((mcp2515_read(MCP_CANSTAT)&MCP_CANSTAT_ICOD_MASK) >> 1);
-		//printf("%u",retval);
 		return retval;
 	}
-	return NOINT;
+	return no_int;
 }
 
 void CAN_int_clear(interrupt CAN_interrupt){
-	if(CAN_interrupt != NOINT){
-		mcp2515_bit_modify(MCP_CANINTF, interruptToMask(CAN_interrupt), 0x00);	
+	if(CAN_interrupt != no_int){
+		mcp2515_bit_modify(MCP_CANINTF, interrupt_to_mask(CAN_interrupt), 0x00);	
 	}
 }
 
@@ -179,15 +163,15 @@ void CAN_all_int_clear(){
 	mcp2515_write(MCP_CANINTF, 0x00);
 }
 
-uint8_t interruptToMask(interrupt CAN_interrrupt){
-	switch(CAN_interrrupt){
-		case NOINT:
+uint8_t interrupt_to_mask(interrupt CAN_interrupt){
+	switch(CAN_interrupt){
+		case no_int:
 			return 0x00;
-		case ERR:
+		case err:
 			return 0b00100000;
-		case RX0:
+		case rx0:
 			return 0b00000001;
-		case RX1:
+		case rx1:
 			return 0b00000010;
 		default:
 			return 0x00;
